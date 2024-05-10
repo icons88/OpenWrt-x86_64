e7f55f6915e647e9ec851f6b6dc752e9e9aff926
OmyK <31337777+icons88@users.noreply.github.com>
2024-05-10 16:58:29 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -4,7 +4,7 @@ sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
 
 # sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt-Firker '/g" ./package/lean/default-settings/files/zzz-default-settings
 sed -i "s/hostname='OpenWrt'/hostname='EopWrt'/g" ./package/base-files/files/bin/config_generate
-
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 # curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/logo.jpg > ./package/luci-app-serverchan/root/usr/bin/serverchan/api/logo.jpg
 
 sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/files/bin/config_generate
