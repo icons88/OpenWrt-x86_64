6b6b1a2d52dbcb341fda7db377e4b2b62e615f6e
OmyK <31337777+icons88@users.noreply.github.com>
2024-07-04 23:35:36 +0800

    Update diy-part.sh

--- 5.15/diy-part.sh
+++ 5.15/diy-part.sh
@@ -10,7 +10,6 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-defaul
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' ./feeds/luci/collections/luci/Makefile
 
-# 增固件连接数
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
 rm -rf ./feeds/luci/applications/luci-app-wechatpush
