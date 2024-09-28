13376354f15e8c9b957f549039a3d726c88d4b94
OmyK <31337777+icons88@users.noreply.github.com>
2024-09-28 22:00:58 +0800

    Update diy6.1.sh

--- 6.1/diy6.1.sh
+++ 6.1/diy6.1.sh
@@ -35,8 +35,6 @@ find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-make menuconfig # choose LUCI -> Applications -> luci-app-mosdns
-make package/mosdns/luci-app-mosdns/compile V=s
 
 # git clone https://github.com/firkerword/luci-app-serverchan.git package/luci-app-serverchan
 # git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
