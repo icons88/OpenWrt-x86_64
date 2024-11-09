12b47357473bf7b4c9f5a553638909b79b092e81
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-09 21:15:08 +0800

    Update diy-part.sh

--- 5.15/diy-part.sh
+++ 5.15/diy-part.sh
@@ -4,9 +4,6 @@ sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' ./feeds/luci/collections/luci/Makefile
-
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
 rm -rf ./feeds/luci/applications/luci-app-wechatpush
