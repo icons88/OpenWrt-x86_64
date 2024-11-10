036f1fb94db6275ef2dd2a602ea81d75090ba47e
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-10 13:34:53 +0800

    Update diy.sh

--- 5.15/diy.sh
+++ 5.15/diy.sh
@@ -19,29 +19,3 @@ rm -rf package/custom; mkdir package/custom
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-#git clone https://github.com/jerrykuku/lua-maxminddb.git  package/lua-maxminddb
-
-# git clone https://github.com/fw876/helloworld.git package/ssr
-
-# git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
-# git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
-# git clone https://github.com/gngpp/luci-theme-neobird.git package/luci-theme-neobird
-# git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-# git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
-# git clone https://github.com/firkerword/smartdns.git package/smartdns
-# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-# find ./ | grep Makefile | grep mosdns | xargs rm -f
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-# make menuconfig # choose LUCI -> Applications -> luci-app-mosdns
-# make package/mosdns/luci-app-mosdns/compile V=s
-
-# git clone https://github.com/firkerword/luci-app-serverchan.git package/luci-app-serverchan
-# git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-# merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
-
