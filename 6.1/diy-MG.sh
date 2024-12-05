9b65fa818b544bbc0d170bed53ee67b8db767964
OmyK <31337777+icons88@users.noreply.github.com>
2024-12-05 14:57:08 +0800

    Update diy-MG.sh

--- 6.1/diy-MG.sh
+++ 6.1/diy-MG.sh
@@ -1,5 +1,6 @@
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
 sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='LEDE'/hostname='MinGwrt'/g" package/base-files/files/bin/config_generate
 sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
@@ -9,3 +10,4 @@ sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
 sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
 sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+
