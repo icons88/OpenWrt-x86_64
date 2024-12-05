767d776165b99c1dbfafca568c8a72c55b26b521
OmyK <31337777+icons88@users.noreply.github.com>
2024-12-05 22:27:53 +0800

    Update diy-MG.sh

--- 6.1/diy-MG.sh
+++ 6.1/diy-MG.sh
@@ -1,8 +1,8 @@
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
-sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='LEDE'/hostname='MinGwrt'/g" package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/files/bin/config_generate
+# sed -i "s/hostname='LEDE'/hostname='MinGwrt'/g" package/base-files/files/bin/config_generate
 sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
